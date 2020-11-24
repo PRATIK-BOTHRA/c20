cbbff6b2e089d521d7e2223c9ecce720ad9058e0
PRATIK-BOTHRA <74529643+PRATIK-BOTHRA@users.noreply.github.com>
2020-11-24 12:50:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var a,b;
+
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 100);
+  b=createSprite(600, 200, 50, 50);
+  a.shapeColor="green";
+  b.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255);  
+  a.x=World.mouseX;
+  a.y=World.mouseY;
+  if(a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2&&a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2){
+    a.shapeColor="red";
+    b.shapeColor="red";
+  }
+  else{
+    a.shapeColor="green";
+    b.shapeColor="green";
+  }
+  console.log(a.x-b.x);
+  drawSprites();
+}
